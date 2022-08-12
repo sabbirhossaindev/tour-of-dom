2eed532634fd8da7b84e7d2268ba98e2300b9ceb
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-13 05:01:48 +0600

    querySelector ........Explore

--- js/dom.js
+++ js/dom.js
@@ -11,8 +11,8 @@ let friends = {
 /*
 toPart note:----------------->
 
-    getElementsByTagName  explore.....
-    getElementById explore......
+    1. getElementsByTagName  explore.....
+    2. getElementById explore...... kno kata specak vava kisu pata chila id use kora lagba.....
 
     innerText   & set value explore......
 
@@ -20,7 +20,24 @@ toPart note:----------------->
     fruits.innerText = 'fruits changed by javaScript'; // innerText   & set value
     console.log(fruits);
 
-    getElementsByClassName explore ....
+    3. getElementsByClassName explore ....
+
+     querySelector ........Explore..
+    1. 4. querySelectorAll  mane holilo all query eloctor add hoba..
+    const container = document.querySelectorAll('.fruits-container li');
+    for (const li of container) {
+        console.log(li.innerText);
+    }
+    2. 5. document.querySelector('.fruits-container li'); only akta select korta .....
+
+   ---------------------->
+   
+   defarance of HTMLCollection  or NodeList home work....
+
+    ----------------------------->
+
+    1. HTMLCollection: live lige....
+    2. NodeList:  static...
 */
 const liCollection = document.getElementsByTagName('li');
     // console.log(liCollection);
@@ -48,3 +65,17 @@ console.log(place);
 
 let importan_places = document.getElementsByClassName('important-palce');
 console.log(importan_places);
+
+
+// querySelector ........Explore..
+// 1. querySelectorAll  mane holilo all query eloctor add hoba..
+// const container = document.querySelectorAll('.fruits-container li');
+// for (const li of container) {
+//     console.log(li.innerText);
+// }
+// 2. document.querySelector('.fruits-container li'); only akta select korta .....
+
+const onlyOne = document.querySelector('.fruits-container li');
+for (const li of onlyOne) {
+    console.log(li.innerText);
+}
