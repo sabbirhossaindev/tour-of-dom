f1c9faf9a52465edbb9dff59aeab4dbe78e90a9b
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-13 03:11:07 +0600

    all link added

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,15 @@
 console.log("Hello form js");
+
+let friends = {
+    name: 'badon',
+    fnd: 'hridoy',
+    id: 477480,
+    study: function (time) {
+        console.log(time, 'study kortasa.......');
+    },
+}
+const liCollection = document.getElementsByTagName('h1');
+    // console.log(liCollection);
+for (const li of liCollection) {
+    console.log(li.innerText);
+}
