18f89b65fb2eedbb478d3ea0ed1bfd8dfc46097e
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-13 04:32:14 +0600

    getElementsByClassName

--- js/dom.js
+++ js/dom.js
@@ -8,9 +8,20 @@ let friends = {
         console.log(time, 'study kortasa.......');
     },
 }
+/*
+toPart note:----------------->
 
-// getElementsByTagName  explore.....
+    getElementsByTagName  explore.....
+    getElementById explore......
 
+    innerText   & set value explore......
+
+    let fruits = document.getElementById('fruits-title');
+    fruits.innerText = 'fruits changed by javaScript'; // innerText   & set value
+    console.log(fruits);
+
+    getElementsByClassName explore ....
+*/
 const liCollection = document.getElementsByTagName('li');
     // console.log(liCollection);
 // for of loop use.........
@@ -24,3 +35,16 @@ const allHeading = document.getElementsByTagName('h1');
 for (const h1 of allHeading) {
     console.log(h1.innerText);
 }
+
+// getElementById  kno kata specak vava kisu pata chila id use kora lagba....
+let fruits = document.getElementById('fruits-title');
+fruits.innerText = 'fruits changed by javaScript'; // innerText   & set value
+console.log(fruits);
+
+
+let place = document.getElementById('places-title').innerText;
+console.log(place);
+
+
+let importan_places = document.getElementsByClassName('important-palce');
+console.log(importan_places);
